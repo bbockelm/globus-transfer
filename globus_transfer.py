dc08fe16a59cd60ee88fc039455776b153bcae42
Josh Karpel <karpel@wisc.edu>
2020-02-18 09:21:58 -0600

    use refresh token flow (with bad storage of the refresh token)

--- globus_transfer.py
+++ globus_transfer.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+from pathlib import Path
 
 import click
 from click_didyoumean import DYMGroup
@@ -12,6 +13,8 @@ logger.setLevel(logging.DEBUG)
 AUTH_ERROR = 1
 
 CLIENT_ID = "fbb557b2-aa0b-42e9-9a07-04c5c4f01474"
+
+REFRESH_TOKEN_PATH = Path.home() / ".globus_refresh_token"
 
 # CLI
 
@@ -45,6 +48,19 @@ def _setup_logger():
 
 
 @cli.command()
+def login():
+    try:
+        refresh_token = acquire_refresh_token()
+        logger.debug("Acquired refresh token")
+    except globus_sdk.AuthAPIError as e:
+        logger.error(f"Was not able to authorize {e.args[-1]}", file=sys.stderr)
+        click.echo(f"ERROR: was not able to authorize", err=True)
+        sys.exit(AUTH_ERROR)
+
+    write_refresh_token(refresh_token)
+
+
+@cli.command()
 def endpoints():
     for ep in get_transfer_client().endpoint_search(filter_scope="my-endpoints"):
         click.echo("[{}] {}".format(ep["id"], ep["display_name"]))
@@ -62,8 +78,12 @@ def history(limit):
 # BACKEND
 
 
-def acquire_auth_and_transfer_data():
-    client = globus_sdk.NativeAppAuthClient(CLIENT_ID)
+def get_client():
+    return globus_sdk.NativeAppAuthClient(CLIENT_ID)
+
+
+def acquire_refresh_token():
+    client = get_client()
     client.oauth2_start_flow(refresh_tokens=True)
 
     click.echo(f"Go to this URL and login: {client.oauth2_get_authorize_url()}")
@@ -71,28 +91,36 @@ def acquire_auth_and_transfer_data():
 
     token_response = client.oauth2_exchange_code_for_tokens(auth_code)
 
-    globus_auth_data = token_response.by_resource_server["auth.globus.org"]
     globus_transfer_data = token_response.by_resource_server["transfer.api.globus.org"]
 
-    return globus_auth_data, globus_transfer_data
+    return globus_transfer_data["refresh_token"]
 
 
-def make_transfer_client(transfer_token):
-    # TODO: refresh tokens
-    authorizer = globus_sdk.AccessTokenAuthorizer(transfer_token)
-    return globus_sdk.TransferClient(authorizer=authorizer)
+def write_refresh_token(refresh_token, path=None):
+    if path is None:
+        path = REFRESH_TOKEN_PATH
+
+    path.write_text(refresh_token)
+    logger.debug(f"Wrote refresh token to {path}")
+
+    return path
+
+
+def read_refresh_token(path=None):
+    if path is None:
+        path = REFRESH_TOKEN_PATH
+
+    token = REFRESH_TOKEN_PATH.read_text().strip()
+    logger.debug(f"Read refresh token from {path}")
+
+    return token
 
 
 def get_transfer_client():
-    try:
-        auth_data, transfer_data = acquire_auth_and_transfer_data()
-        logger.debug("Acquired transfer token")
-    except globus_sdk.AuthAPIError as e:
-        logger.error(f"Was not able to authorize {e.args[-1]}", file=sys.stderr)
-        click.echo(f"ERROR: was not able to authorize", err=True)
-        sys.exit(AUTH_ERROR)
-
-    return make_transfer_client(transfer_data["access_token"])
+    refresh_token = read_refresh_token()
+    client = get_client()
+    authorizer = globus_sdk.RefreshTokenAuthorizer(refresh_token, client)
+    return globus_sdk.TransferClient(authorizer=authorizer)
 
 
 if __name__ == "__main__":
